26380fbc24fc1bcab94758f012cf2a1db7f0a0a0
Steven Buri <steve.buri@gmail.com>
2022-11-18 08:35:49 -0500

    new .py created to house test and example dataframes

---
+++ TestDataFrames.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dfsimple = pd.DataFrame({
+    'sequential_numbers': [1, 2, 3, 4, 5],
+    'text': ['This', 'Is', 'Always', 'A', 'Test'],
+    'sum_numbers': [5, 10, 15, 20, 25],
+})
