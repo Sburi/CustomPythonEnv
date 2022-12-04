9e406ed839a838290fccbcc9032b3dc74eab0853
Sburi <41414125+Sburi@users.noreply.github.com>
2022-12-04 16:36:14 -0600

    added dfwithblanks

--- TestDataFrames.py
+++ TestDataFrames.py
@@ -1,7 +1,15 @@
+import numpy as np
 import pandas as pd
 
 dfsimple = pd.DataFrame({
     'sequential_numbers': [1, 2, 3, 4, 5],
     'text': ['This', 'Is', 'Always', 'A', 'Test'],
+    'sum_numbers': [5, 10, 15, 20, 25],
+})
+
+
+dfwithblanks = pd.DataFrame({
+    'sequential_numbers': [1, 2, 3, 4, 5],
+    'text': ['This', '', 'A', np.nan, 'Test'],
     'sum_numbers': [5, 10, 15, 20, 25],
 })
