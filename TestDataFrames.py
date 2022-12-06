21c1367806b1b6c49ee3958d9f08f1ebe7bee577
Steven Buri <steve.buri@gmail.com>
2022-12-05 18:16:02 -0600

    added df for binning. Eventually will want to conslidate dfs to one semi-complicated but predictable one.

--- TestDataFrames.py
+++ TestDataFrames.py
@@ -13,3 +13,8 @@ dfwithblanks = pd.DataFrame({
     'text': ['This', '', 'A', np.nan, 'Test'],
     'sum_numbers': [5, 10, 15, 20, 25],
 })
+
+dfforbins = pd.DataFrame({
+    'Age': [1, 2, 3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 7, 8, 10, 40, 80],
+    'Survived': [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+})
