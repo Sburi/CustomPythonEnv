65c96114c399f5565037e1644a9cf720f738f253
Steven Buri <steve.buri@gmail.com>
2023-03-14 13:23:33 -0400

    added QAOneDataFrame, QATwoDataFrames

--- QA.py
+++ QA.py
@@ -5,6 +5,10 @@ from TestDataFrames import dfsimple
 from Formats import Formats
 fmt = Formats()
 
+from IPython.display import display, HTML
+
+from types import SimpleNamespace
+
 class QARawvsFinal:
     '''
     Tests if raw sums equals final sum.
@@ -119,39 +123,182 @@ class QARawvsFinal:
             variance = f'Variance: {variance}'
             return print(result, start, final, variance, '', sep='\n')
 
+class QAOneDataFrame:
+    '''
+    Purpose
+    -----------
+        Perform tests related to a single dataframe
 
-    # def compare_dataframes(self, col):
-    #     #round
-    #     dfmerge[col_mainforecasttable] = dfmerge[col_mainforecasttable].round(0)
-    #     dfmerge[col_dlforecasttable] = dfmerge[col_dlforecasttable].round(0)
+    Parameters
+    ------------
+        df: pd.DataFrame
+            The dataframe you want to test
+    '''
 
-    #     #create variance column
-    #     dfmerge[col_variance] = dfmerge[col_mainforecasttable] - dfmerge[col_dlforecasttable]
-    #     dfmerge[col_variance] = dfmerge[col_variance].round(0)
-    #     dfmerge[col_variance] = dfmerge[col_variance].fillna(0)
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
 
-    #     #anaylze
-    #     dfmerge.loc[
-    #         dfmerge[col_variance].round(0)!=0,
-    #     'Equal'] = 'FALSE'
+    def check_percentage_equals_to_100_percent(self, column_to_groupby: str, column_with_percentage: str):
+        '''
+        Purpose
+        -----------
+            Tests if a target column is fully distributed.
 
-    #     #filter
-    #     dfmerge = dfmerge[dfmerge['Equal'] == 'FALSE']
-    #     dfmerge = dfmerge.drop(columns=['Equal'])
+        Parameters
+        ------------
+            column_to_groupby: str
+                The column which should be grouped such that the corresponding percentage column totals to 100% when grouped by this item.
+            column_with_percentage: str
+                The column with the percentages.
+        '''
 
-    #     #show results
-    #     if len(dfmerge)==0:
-    #         print('|✓| Staying in pre-existing org matches between main and direct labor tables.')
-    #     else:
-    #         print('|X| Staying in pre-existing org does not between main and direct labor tables.\n')
+        dfgrouped = self.df.groupby(by=column_to_groupby)[column_with_percentage].sum().reset_index()
+        dfnotfullydistributed = dfgrouped[dfgrouped[column_with_percentage].round(2)!=1]
+
+        if len(dfnotfullydistributed)!=0:
+            print('|X| The following items have less than 100% distribution:')
+            display(HTML(dfnotfullydistributed.to_html()))
+        else:
+            print('|✓| All items were 100% distributed.')
+
+    def check_equivalence(self, all_columns_to_check: list, columns_to_combine: str, column_quantified_columns_should_equal: str):
+        '''
+        Purpose
+        ------------
+            Checks equivalence of given columns.
+
+        Inputs
+        -----------
+            all_columns_to_check: list
+                All the columns you want to check as strings in a list.
+            columns_to_combine: str
+                The columns you want to combine prior to checking equality with some other column, expressed as a single string. Each column must be started with "n."
+                Ex: 'n.ytd_actuals - n.ytd_plan'
+            column_quantified_columns_should_equal: str
+                The column you want to check equality with, expressed as a string, prepended with "n."
+                Ex: 'n.ytd_varaince'
+        '''
         
-    #         #format columns
-    #         columns = [col_mainforecasttable, col_dlforecasttable]
-    #         for col in columns:
-    #             dfmerge[col] = dfmerge[col].fillna(0)
-    #             dfmerge[col] = dfmerge[col].apply(lambda x: f"${x*1:,.0f}")
-    #         print(dfmerge)
+        d_sums = {}
+        for col in all_columns_to_check:
+            #name sum
+            sum_name = col
+            
+            #obtain sum
+            col = self.df[col].sum()
+            col = round(col, 2)
 
+            #store sum
+            d_sums[sum_name] = col
+
+        n = SimpleNamespace(**d_sums)
+        
+        #obtain result of columns to combine
+        result = eval(columns_to_combine)
+        result = round(result, 2)
+
+        #obtain expected result, the equality column
+        expected_result = eval(column_quantified_columns_should_equal)
+        expected_result = round(expected_result, 2)
+
+        if result==expected_result:
+            print(f'|✓| The following logical relationship between the columns is true: {columns_to_combine} == {column_quantified_columns_should_equal}')
+        else:
+            print(f'|X| The following logical relationship between the columns is NOT true: {columns_to_combine} == {column_quantified_columns_should_equal}')
+            print('columns to combine equals: ', result)
+            print('expected value was: ', expected_result)
+
+class QATwoDataFrames:
+
+    '''
+    Use to compare various factors across two dataframes.
+
+    Inputs
+    ------------
+        df1: pd.DataFrame
+            The first dataframe you want to run QA on.
+        df2: pd.DataFrame
+            The second dataframe you want to run QA on.
+    '''
+
+    def __init__(self, df1: pd.DataFrame, df2: pd.DataFrame):
+        self.df1 = df1
+        self.df2 = df2
+    
+    def compare_valuecolumn_acrossgroups(self, df1_groupby_columns: list[str], df2_groupby_columns: list[str], df1_value_column: str, df2_value_column: str):
+
+        '''
+        Combines two dataframes and compares single value column across dataframes.
+
+        Inputs
+        ------------
+            df1_groupby_column: list[str]
+                The name of the columns you want to group by in the classes first dataframe.
+            df2_groupby_column: list[str]
+                The name of the columns you want to group by in the classes second dataframe. MUST be in the same order as the first groupby columns for when column names are standardized across dataframes.
+            df1_value_column: str
+                The name of the value column in the classes first dataframe.
+            df2_value_column: str
+                The name of the value column in the classes second dataframe.
+        
+        '''
+
+        def standardize_value_column_name():
+            store_df_name = self.df1.name
+            self.df1 = self.df1.rename(columns={df1_value_column: df2_value_column})
+            self.df1.name = store_df_name #name gets removed during column rename
+
+        def standardize_group_by_column_name():
+            i=0
+            
+            for col in df1_groupby_columns:
+                #store name
+                store_df_name = self.df1.name
+                
+                #rename columns in df1 to match df2 columns
+                rename_to = df2_groupby_columns[i]
+                self.df1 = self.df1.rename(columns={col: rename_to})
+                i += 1
+
+                #add name back
+                self.df1.name = store_df_name #name gets removed during column rename
+
+        def add_dataframe_source_column():
+            self.df1['df_source'] = self.df1.name
+            self.df2['df_source'] = self.df2.name
+
+        def combine_dataframes():
+            self.dfcombined = pd.concat([self.df1, self.df2])
+
+        def round_value_column():
+            self.dfcombined[df2_value_column] = self.dfcombined[df2_value_column].round(2)
+        
+        def groupby_and_compare():
+            columns_to_groupby = df2_groupby_columns + ['df_source']
+            self.dfcombined = self.dfcombined.groupby(columns_to_groupby)[df2_value_column].sum().reset_index()
+
+        def pivot_source():
+            self.dfcombined = pd.pivot_table(data=self.dfcombined, index=df2_groupby_columns, columns='df_source', values=df2_value_column).reset_index()
+            self.dfcombined.columns.name = None
+
+        def add_variance_column():
+            self.dfcombined['variance'] = self.dfcombined[self.df2.name] - self.dfcombined[self.df1.name]
+
+        def run_pipeline():
+            standardize_value_column_name()
+            standardize_group_by_column_name()
+            add_dataframe_source_column()
+            combine_dataframes()
+            round_value_column()
+            groupby_and_compare()
+            pivot_source()
+            add_variance_column()
+
+            return self.dfcombined
+
+        run_pipeline()
+        return self.dfcombined
+        
 
 if __name__=='__main__':
     def test_qa_rawvsfinal(df: pd.DataFrame):
@@ -165,5 +312,27 @@ if __name__=='__main__':
         filter = (df.text != 'This')
         
         QARawvsFinal(dfraw=df, dffinal=dffinal).compare_starting_vs_final_nongrouped(raw_filter_criteria=filter, starting_col_name='sum_numbers', ending_col_name='sum_numbers', is_currency=False)
+    #test_qa_rawvsfinal(df=dfsimple)
 
-    test_qa_rawvsfinal(df=dfsimple)
+    def test_qatwodataframes():
+        df1 = pd.DataFrame({
+        'Column1': ['A', 'B', 'C', 'D'],
+        'ColumnA': ['Bob', 'Sally', 'Rex', 'John'],
+        'Column3': ['This', 'Should', 'Be', 'Ignored'],
+        'ValueCol_oldname': [200, 300, 400, 500]
+        })
+
+        df2 = pd.DataFrame({
+            'Column1': ['A', 'B', 'C', 'D'],
+            'Column2': ['Bob', 'Sally', 'Rex', 'John'],
+            'Column3': ['This', 'Should', 'Be', 'Ignored'],
+            'ValueCol': [200, 350, 450, 500]
+        })
+
+        df1.name = 'df1'
+        df2.name = 'df2'
+
+        dftest = QATwoDataFrames(df1=df1, df2=df2).compare_valuecolumn_acrossgroups(df1_groupby_columns=['Column1', 'ColumnA'], df2_groupby_columns=['Column1', 'Column2'], df1_value_column='ValueCol_oldname', df2_value_column='ValueCol')
+        print(dftest)
+
+    test_qatwodataframes()
