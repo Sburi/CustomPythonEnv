ab882252045c92686da8885311349d45674bf484
Steven Buri <steve.buri@gmail.com>
2023-03-16 13:25:36 -0400

    moved inputs to class rather than methods.

--- Formats.py
+++ Formats.py
@@ -5,15 +5,19 @@ class Formats:
     '''
     Purpose
     ----------
-        Enable easier formatting to currency, percents, etc for individual numbers.
+        Enable easier formatting of individual numbers to currency, percents, etc.
 
     Parameters
     ----------
-        No class inputs.
+        numbers_to_convert: list
+            A list with the numbers you want to convert.
 
     '''
     
-    def currency(self, number: float):
+    def __init__(self, numbers_to_convert: list):
+        self.numbers_to_convert = numbers_to_convert
+
+    def currency(self):
         '''
         Purpose
         ----------
@@ -21,24 +25,47 @@ class Formats:
 
         Parameters
         ----------
-            number: float
-                The number you want to convert into currency
+            None
 
         Outputs
         ----------
-            A integer formatted as US currency.
+            A list of converted numbers (if conversion possible in every case) or converted/non-converted numbers if unable to convert some/all numbers. 
+
+        Notes
+        ----------
+            To convert a column to a nicer format, you may use the following:
+                pd.options.display.float_format = '{:,.0f}'.format
+            The following conversions have been tried to convert columns to a nicer format but did not work
+                1
+                df[col] = df[col].astype('str').apply(format(','))
+
+                2
+                df[col] = df[col].astype('str').str.format(',')
+
+                3
+                import locale
+                locale.setlocale( locale.LC_ALL, 'English_United States.1252')
+                locale._override_localeconv = {'n_sign_posn':1}
+                df[col] = df[col].map(locale.currency)
+
+                4
+                df[col] = fmt.currency(self.dfcombined[col])
         
         '''
         
-        try:
-            number_rounded = int(round(number))
-            number_as_currrency = '${:,.0f}'.format(number_rounded)
-            #print(number, '>>', number_as_currrency)
-            return number_as_currrency
-        except ValueError:
-            return number
+        converted_numbers = []
+        for number in self.numbers_to_convert:
+        
+            try:
+                number_rounded = int(round(number))
+                number_as_currrency = '${:,.0f}'.format(number_rounded)
+                converted_numbers.append(number_as_currrency)
+            except (ValueError, TypeError):
+                converted_numbers.append(number)
 
-    def percents(self, number: float, n_decimals: int):
+        return converted_numbers
+
+    def percents(self, n_decimals: int):
         '''
         Purpose
         ----------
@@ -46,18 +73,29 @@ class Formats:
 
         Parameters
         ----------
-            number: float
-                The number you want to convert into a percent
             n_decimals: int
                 The number of decimals you want the percentage to have.
 
         Outputs
         ----------
-            A integer formatted as a percent with n decimals.
+            A list of converted numbers (if conversion possible in every case) or converted/non-converted numbers if unable to convert some/all numbers.
         
         '''
         
-        #number = "%.0f%%" % (100 * number) trying to implement number of decimals as below
+        converted_numbers = []
+        for number in self.numbers_to_convert:
+            number = f"%.{n_decimals}f%%" % (100 * number)
+            converted_numbers.append(number)
 
-        number = f"%.{n_decimals}f%%" % (100 * number)
-        return number
+        return converted_numbers
+
+if __name__ == '__main__':
+    def test_currency_conversion():
+        numbers = [1, 2, '3a']
+
+        result = Formats(numbers_to_convert=numbers).currency()
+
+        print(result)
+
+    test_currency_conversion()
+
